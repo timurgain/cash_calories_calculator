0e2e189a4f63111e34605f45a5740fba1363caff
Timur Gain <timur.gain@gmail.com>
2021-10-11 23:11:09 +0300

    Begining

--- homework.py
+++ homework.py
@@ -3,31 +3,33 @@ import datetime as dt
 
 class Record:
     """Describtion."""
-    
+
     pass
 
 
 class Calculator:
     """Describtion."""
 
-    def __init__(self, records, limit: int) -> None:
+    def __init__(self, limit: int) -> None:
         self.limit = limit
         self.records = []
 
     def add_record(ammount: int) -> str:
         pass
-    
+
     def get_today_stats():
         pass
 
 
 class CaloriesCalculator(Calculator):
     """Describtion."""
-    
+
+    super().__init__()
     pass
 
 
 class CashCalculator(Calculator):
     """Describtion."""
-    
+
+    super().__init__()
     pass
