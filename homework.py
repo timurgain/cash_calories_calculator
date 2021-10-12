a61fbb6a7b26b53cc0ff839cf2313111c5a01026
Timur Gain <timur.gain@gmail.com>
2021-10-13 00:42:26 +0300

    Class Record

--- homework.py
+++ homework.py
@@ -5,15 +5,19 @@ from typing import List, Optional
 class Record:
     """Describtion."""
 
-    def __init__(self, amount: int, comment: str, date: Optional[str] = None) -> None:
+    def __init__(self,
+                 amount: int,
+                 comment: str,
+                 date: Optional[str] = None) -> None:
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.date = dt.datetime.utcnow() + dt.timedelta(hours=3)
+            self.date = dt.datetime.utcnow().date() + dt.timedelta(hours=3)
         else:
             date_format = '%d.%m.%Y'
             user_moment = dt.datetime.strptime(date, date_format)
             self.date = user_moment.date()
+        print(self.date)
 
     def get_record(self) -> List:
         record_data = [self.amount, self.comment, self.date]
@@ -24,15 +28,14 @@ class Calculator:
     """Describtion."""
 
     def __init__(self, limit: int) -> None:
+        self.records = []
         self.limit = limit
 
-    def add_record(record: Record):
-        records = []
+    def add_record(self, record: Record):
         rec = record.get_record
-        records = records.append[rec]
-        return records
+        self.records.append(rec)
 
-    def get_today_stats():
+    def get_today_stats(self):
         pass
 
 
@@ -50,3 +53,8 @@ class CashCalculator(Calculator):
     def __init__(self, limit: int) -> None:
         super().__init__(self, limit)
     pass
+
+
+rec1 = Record(33, 'wow')
+r = rec1.get_record
+print('done')
