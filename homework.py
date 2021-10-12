67791f275d312569da93d2301f41f3d10c2a1a00
Timur Gain <timur.gain@gmail.com>
2021-10-13 00:01:45 +0300

    Class Record

--- homework.py
+++ homework.py
@@ -1,20 +1,36 @@
 import datetime as dt
+from typing import List, Optional
 
 
 class Record:
     """Describtion."""
 
-    pass
+    def __init__(self, amount: int, comment: str, date: Optional[str] = None) -> None:
+        self.amount = amount
+        self.comment = comment
+        if date is None:
+            self.date = dt.datetime.utcnow() + dt.timedelta(hours=3)
+        else:
+            date_format = '%d.%m.%Y'
+            user_moment = dt.datetime.strptime(date, date_format)
+            self.date = user_moment.date()
+
+    def get_record(self) -> List:
+        record_data = [self.amount, self.comment, self.date]
+        return record_data
 
 
 class Calculator:
     """Describtion."""
 
+    global records = []
+
     def __init__(self, limit: int) -> None:
         self.limit = limit
-        self.records = []
 
-    def add_record(ammount: int) -> str:
+    def add_record(record: Record):
+        r = record.get_record
+        records = records.append[r]
         pass
 
     def get_today_stats():
@@ -24,12 +40,14 @@ class Calculator:
 class CaloriesCalculator(Calculator):
     """Describtion."""
 
-    super().__init__()
+    def __init__(self, limit: int) -> None:
+        super().__init__(limit)
     pass
 
 
 class CashCalculator(Calculator):
     """Describtion."""
 
-    super().__init__()
+    def __init__(self, limit: int) -> None:
+        super().__init__(self, limit)
     pass
